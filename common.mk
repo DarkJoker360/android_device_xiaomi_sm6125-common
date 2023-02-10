3cf1702edef5433a0833fec3146b36c30797969c
DarkJoker360 <simoespo159@gmail.com>
2023-02-10 12:32:39 +0100

    sm6125-common: Remove audio.a2dp.default
    * Built by default.
    
    Change-Id: I873a83edec3349d28dd3741acb3db6337846a26f

--- common.mk
+++ common.mk
@@ -95,7 +95,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.effect@2.0-service \
     android.hardware.soundtrigger@2.2-impl \
-    audio.a2dp.default \
     audio.primary.trinket \
     audio.r_submix.default \
     audio.usb.default \
